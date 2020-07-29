3f8e07093c7fcabff278cbe7c40e12707022a96e
Erkan Ugurlu <ervanux@gmail.com>
2020-07-29 12:58:22 +0300

    Tried with b-tree still no valid solution for a long sentence. it takes O(n!)

--- WordBreak.playground/Contents.swift
+++ WordBreak.playground/Contents.swift
@@ -1,61 +1,73 @@
 /// https://leetcode.com/problems/word-break/
 
-final class Solution {
-    func wordBreak2(_ sub: Substring, _ wordDict: [String], start: Int, stop: Int) -> Bool {
-        if sub.isEmpty {
-            return true
-        }
-        
-        if start >= wordDict.count || stop >= wordDict.count {
-            return false
-        }
-        
-        var newSub = sub
-        
-        var newStart = start
-        let startWord = wordDict[newStart]
-        if newSub.prefix(startWord.count) == startWord {
-            let nextStartIndex = newSub.index(newSub.startIndex, offsetBy: startWord.count)
-            if nextStartIndex == newSub.index(before: newSub.endIndex) {
-                return true
-            }
-            newSub = newSub[nextStartIndex...]
-            newStart = 0
-        } else {
-            newStart += 1
-        }
-        
-        var newStop = stop
-        let stopWord = wordDict[newStop]
-        if newSub.suffix(stopWord.count) == stopWord {
-            let nextStopIndex = newSub.index(newSub.endIndex, offsetBy: -(stopWord.count))
-            if nextStopIndex == newSub.startIndex {
-                return true
-            }
-            newSub = newSub[..<nextStopIndex]
-            newStop = 0
-            newStart = 0
-        } else {
-            newStop += 1
-        }
+struct Node{
+    let wordIndex: Int
+    var children = [Node]()
+    
+    mutating func add(child: Node) {
+        children.append(child)
+    }
+}
 
-        return wordBreak2(newSub, wordDict, start: newStart, stop: newStop)
-        
+final class Solution {
+    private var wordDict: [String]!
+    var root = Node(wordIndex: -1)
+
+    func wordBreak2(_ deep: Int, _ sub: Substring, _ wordDict: [String]) -> Bool {
+        for word in wordDict where sub.starts(with: word) {
+            let pointer = sub.index(sub.startIndex, offsetBy: word.count)
+            if pointer == sub.endIndex {
+                return true
+            }
+            if wordBreak2(deep + 1,sub[pointer...], wordDict) {
+                return true
+            }
+        }
+        return false
     }
 
     func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-        return wordBreak2(s[s.startIndex...], wordDict, start: 0,stop: 0)
+        let first = Set(s.unicodeScalars)
+        let second = Set(wordDict.joined().unicodeScalars)
+        if first.count > second.count {
+            return false
+        }
+        if !first.isSubset(of: second) {
+            return false
+        }
+        
+        let sorted = wordDict.sorted { $0.count > $1.count }
+        var array = [String]()
+        for (index, word) in sorted.enumerated() {
+            var new = sorted
+            new.remove(at: index)
+            if !wordBreak2(1, word[word.startIndex...], new) {
+                array.append(word)
+            }
+        }
+        
+        self.wordDict = array
+        fillChild(s: s[s.startIndex...], root: &root)
+        return false
+    }
+    
+    func fillChild(s: Substring, root: inout Node) {
+        for (index, word) in wordDict.enumerated() where s.starts(with: word) {
+            var child = Node(wordIndex: index)
+            fillChild(s: s[s.index(s.startIndex, offsetBy: word.count)...], root: &child)
+            root.add(child: child)
+        }
     }
 }
 
 //Solution().wordBreak("leetcode", ["leet","code"])
 //Solution().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"])
 //////
-//Solution.init().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",["aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"])
+Solution.init().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",["aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa","ba"])
 //////
 //Solution().wordBreak("bb", ["a","b","bbb","bbbb"])
 ////////
 //Solution().wordBreak("cars",["car","ca","rs"])
 //Solution().wordBreak("catsandog", ["cats","dog","sand","and","cat"])
 
-Solution().wordBreak("goalspecial", ["go","goal","goals","special"])
+//Solution().wordBreak("goalspecial", ["go","goal","goals","special"])
